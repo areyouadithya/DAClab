79fe23581d20deaef81a12c9863a331789ce0c75
Adithya R U <112653455+areyouadithya@users.noreply.github.com>
2024-03-06 14:16:14 +0530

    Add files via upload

---
+++ s4feb21_rmi/SearchQuery.java
@@ -0,0 +1,26 @@
+import java.rmi.*;
+import java.rmi.server.*;
+public class SearchQuery extends UnicastRemoteObject
+						implements Search
+{
+	// Default constructor to throw RemoteException
+	// from its parent constructor
+	SearchQuery() throws RemoteException
+	{
+		super();
+	}
+
+	// Implementation of the query interface
+	public String query(String search)
+					throws RemoteException
+	{
+		String result;
+		if (search.equals("Reflection in Java"))
+			result = "Found";
+		else
+			result = "Not Found";
+
+		return result;
+	}
+}
+
